6cd06edb7608ef78cbaa52a582edff27a890fde1
prathamesh <rajeshchoudhury098@gmail.com>
2025-02-06 16:01:04 +0530

    new change

--- lib/models/category2.dart
+++ lib/models/category2.dart
@@ -2,12 +2,12 @@ class Category {
   final String id;
   final String name;
   final String imageUrl;
-  final List<String> subcategories; // Add subcategories
+  final List<String> subcategories;
 
   Category({
     required this.id,
     required this.name,
     required this.imageUrl,
-    required this.subcategories, // Add subcategories in constructor
+    required this.subcategories,
   });
 }
